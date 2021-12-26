7e271b75b33da40c3526a2ac254264d9e6fa75a9
jiwpark00 <jiwpark00@gmail.com>
2021-12-26 12:09:45 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package HelloWorld;
+
+public class Main {
+  public static void main(String[] args) {
+    // Print the words Hello World on the screen
+    System.out.println("Hello World");
+  }
+}
