32717c0ad019790ae266ea1fd4192843dd2b9a02
Ji Woong Park <jiwpark00@gmail.com>
2022-12-30 19:48:05 -0500

    stage 3 completed

--- Coffee Machine.java
+++ Coffee Machine.java
@@ -1,9 +1,11 @@
 package machine;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        // First Stage
         // System.out.println("Hello World!");
         // String coffeeText = """
         //     Starting to make a coffee
@@ -16,12 +18,39 @@ public class CoffeeMachine {
         //     """;
         // System.out.println(coffeeText);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
-        int coffeeNeeds = scanner.nextInt();
+
+        // Second Stage
+        // System.out.println("Write how many cups of coffee you will need:");
+        // int coffeeNeeds = scanner.nextInt();
         
-        System.out.printf("For %d cups of coffee you will need:\n", coffeeNeeds);
-        System.out.printf("%d ml of water\n", 200*coffeeNeeds);
-        System.out.printf("%d ml of milk\n", 50*coffeeNeeds);
-        System.out.printf("%d g of coffee beans\n", 15*coffeeNeeds);
+        // System.out.printf("For %d cups of coffee you will need:\n", coffeeNeeds);
+        // System.out.printf("%d ml of water\n", 200*coffeeNeeds);
+        // System.out.printf("%d ml of milk\n", 50*coffeeNeeds);
+        // System.out.printf("%d g of coffee beans\n", 15*coffeeNeeds);
+
+        // Third Stage
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int availWater = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int availMilk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int availBeans = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int neededCoffee = scanner.nextInt();
+
+        int maxPossibleCoffee = Math.min(availWater / 200, Math.min(availMilk / 50, availBeans / 15));
+        
+        if (maxPossibleCoffee < neededCoffee) {
+            System.out.printf("No, I can make only %d cup(s) of coffee\n", maxPossibleCoffee);
+        }
+        else if (maxPossibleCoffee == neededCoffee) {
+            System.out.printf("Yes, I can make that amount of coffee");
+        }
+        else {
+            int remainCoffee = maxPossibleCoffee - neededCoffee;
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", remainCoffee);
+        }
     }
 }
