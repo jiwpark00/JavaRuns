42a283dc7cc91e610e42a1d765f63a0542b8ef21
Ji Woong Park <jiwpark00@gmail.com>
2023-01-02 23:24:05 -0500

    updated with Stage 3

--- Cinema Room Manager.java
+++ Cinema Room Manager.java
@@ -19,8 +19,30 @@ public class Cinema {
         // System.out.println("7 S S S S S S S S");
 
         // Second Stage
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
         
+        // System.out.println("Enter the number of rows: ");
+        // int rowCounts = scanner.nextInt();
+        // System.out.println("Enter the number of seats in each row: ");
+        // int seatCounts = scanner.nextInt();
+
+        // int totSeats = rowCounts * seatCounts;
+
+        // System.out.println("Total income:");
+        
+        // if (totSeats < 60) {
+        //     System.out.println("$" + totSeats*10);
+        // }
+        // else {
+        //     int firstProfits = rowCounts/2*seatCounts*10;
+        //     int secondProfits = (rowCounts - rowCounts/2)*seatCounts*8;
+        //     int totProfits = firstProfits + secondProfits;
+        //     System.out.println("$" + totProfits);
+        // }
+
+        // Third Stage
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the number of rows: ");
         int rowCounts = scanner.nextInt();
         System.out.println("Enter the number of seats in each row: ");
@@ -28,17 +50,74 @@ public class Cinema {
 
         int totSeats = rowCounts * seatCounts;
 
-        System.out.println("Total income:");
-        
-        if (totSeats < 60) {
-            System.out.println("$" + totSeats*10);
+        System.out.println("Cinema:");
+        for (int i = 0; i < rowCounts+1; i++) {
+            if (i == 0) {
+                System.out.print(" ");
+                for (int j = 1; j < seatCounts+1; j++) {
+                    System.out.print(" " + j);
+                    if (j == seatCounts) {
+                        System.out.println("");
+                    }
+                }
+            }
+            else {
+                System.out.print(i + " ");
+                String sVals = "S ".repeat(seatCounts-1) + "S";
+                System.out.print(sVals);
+                System.out.println("");
+            }
         }
+
+        System.out.println("");
+        System.out.println("Enter a row number: ");
+        int rowCountsSeat = scanner.nextInt();
+        System.out.println("Enter a seat number in that row: ");
+        int seatCountsSeat = scanner.nextInt();
+
+        if (totSeats < 60) {
+            System.out.println("Ticket price: $10");
+        }
+
         else {
-            int firstProfits = rowCounts/2*seatCounts*10;
-            int secondProfits = (rowCounts - rowCounts/2)*seatCounts*8;
-            int totProfits = firstProfits + secondProfits;
-            System.out.println("$" + totProfits);
+            int midPoint = rowCounts/2;
+            if (rowCountsSeat <= midPoint) {
+                System.out.println("Ticket price: $10");
+            }
+            else {
+                System.out.println("Ticket price: $8");
+            }
+        }
+        
+        System.out.println("Cinema:");
+        for (int i = 0; i < rowCounts+1; i++) {
+            if (i == 0) {
+                System.out.print(" ");
+                    for (int j = 1; j < seatCounts+1; j++) {
+                        System.out.print(" " + j);
+                        if (j == seatCounts) {
+                            System.out.println("");
+                        }
+                    }
+                }
+            else {
+                if (i != rowCountsSeat) {
+                    System.out.print(i + " ");
+                    String sVals = "S ".repeat(seatCounts-1) + "S";
+                    System.out.print(sVals);
+                    System.out.println("");    
+                    }
+                else {
+                    System.out.print(i + " ");
+                    String sValsWithB = "S ".repeat(seatCountsSeat-1) + "B " +
+                        "S ".repeat(seatCounts-seatCountsSeat-1) + "S";
+                    System.out.print(sValsWithB);
+                    System.out.println("");
+                    }
+            }
+        }
+
+            
         }
         
     }
-}
